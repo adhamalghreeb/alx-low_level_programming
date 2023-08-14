e9fa73c9a2d1bdc9cdbf3e623f1c56a885e1867f
adhamghreeb <adhamghreeb@gmail.com>
2023-08-14 21:44:35 +0300

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,15 +10,21 @@
 
 int main(void)
 {
-int digit = 0;
-while (digit <= 9){
-putchar(digit + 48);
-if (digit != 9)
-{putchar(',');
-putchar(' ');
-}
-++digit;
-}
-putchar('\n');
-return (0);
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + 48);
+
+		if (digit != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+		++digit;
+	}
+	putchar('\n');
+
+	return (0);
 }
